53da49a5895e93a8f38ee496d46daca7bb807762
flczin <64979207+flczin@users.noreply.github.com>
2024-02-27 21:05:16 -0300

    :sparkles: feat: header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header(){
+    return (
+        <header className="flex justify-between items-center p-3 border-b-2 border-border">
+            <span className="text-2xl font-bold ">Best Movies</span>
+        </header>
+    )
+}
