17f2f13dae38dca20eb91cd4af1bfb49112a8691
flczin <64979207+flczin@users.noreply.github.com>
2024-02-27 21:05:24 -0300

    :sparkles: feat: footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <footer className="flex justify-end items-center p-3 border-t-2 border-border">
+            <span className="text-base">By flczin.</span>
+        </footer>
+    )
+}
